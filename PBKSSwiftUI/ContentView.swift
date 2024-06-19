be3c5c53fad05ec0defdba482c03eee6938b1e07
Apurva Jalgaonkar <apurva.jalgaonkar@sportzinteractive.net>
2024-06-19 11:36:09 +0530

    Initial Commit

---
+++ PBKSSwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PBKSSwiftUI
+//
+//  Created by Apurva Jalgaonkar on 19/06/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PBKSSwiftUI/PBKSSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PBKSSwiftUIApp.swift
+//  PBKSSwiftUI
+//
+//  Created by Apurva Jalgaonkar on 19/06/24.
+//
+
+import SwiftUI
+
+@main
+struct PBKSSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
